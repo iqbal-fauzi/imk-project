b02929709f64b460a0c234f2b096bf80350ca366
Natrium <Natrium@Natrium-PC>
2017-05-17 17:48:49 +0700

    # init project

---
+++ footer.php
@@ -0,0 +1,54 @@
+<footer>
+<!--<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+                <p>� Copyright 2017 Site</p>
+        </div>
+    </div>
+</div>-->
+    <div id="footer">
+			<div class="footer-menu container-fluid">
+				<div class="row">
+					<div class="col-sm-12 col-xs-12">
+						<div class="menu">
+                            
+                            <div class="col-sm-4  col-xs-12">
+								<div class="box">
+                                    <img src="image/umb.jpg" width="150" height="150" />
+								</div>
+							</div>
+                            
+							<ul class="main-menu col-sm-4 text-center">
+								<li><?php echo 'hthtyht' ?></li>
+								<li><?php echo 'ythyth' ?></li>
+								<li><?php echo 'dsds' ?></li>
+							</ul>
+							<div class="col-sm-4  col-xs-12">
+								<div class="box">
+										<p>
+											<i class="fa fa-map-marker"></i>
+											Alamat:
+											Jl. Menteng Raya No.29, Jakarta Pusat<br>
+											Telepon/Faximile: 021 3193 5454
+										</p>
+										<p>
+											Email
+											mercumenteng@gmail.com
+										</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+            <br/>
+            <br/>
+            <br/>
+			<p>
+				Tugas IMK <?php echo date('Y'); ?>
+			</p>
+			<a href="#" class="gototop">
+				<i class="fa fa-arrow-up"></i>
+			</a>
+		</div>
+</footer>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Site</title>
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/slider.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<script src="test/main.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="owl/dist/assets/owl.carousel.min.css" />
+<script src="owl/dist/owl.carousel.min.js"></script>
+<script src="js/slider.js"></script>
+<script>
+</script>
+
+</head>
+<body>
+<nav id="myNavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Site</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#" target="_blank">Home</a></li>
+                <li><a href="#" target="_blank">About</a></li>
+                <li><a href="#" target="_blank">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!--<section>-->
+    <div id="slider">
+      <a href="#" class="control_next">></a>
+      <a href="#" class="control_prev"><</a>
+      <ul>
+          <li><img src="image/un.jpg"</li>
+          <li><img src="image/har.png"</li>
+          <li><img src="image/lon.jpg"</li>
+          <li><img src="image/ny.jpg"</li>
+      </ul>  
+    </div>
+
+<!--<div class="slider_option">
+  <input type="checkbox" id="checkbox">
+  <label for="checkbox">Autoplay Slider</label>
+</div> -->
+
+
+<!--</section>-->
+    
+	<section style="background-color: gray;">
+        <div class="container text">
+			<p>dkslhdjsahdalskdaskjdskaldhnjslkahdkijwhlfkdiuhkjawdhsasdfghjk</p>
+			<p>dkslhdjsahdalskdaskjdskaldhnjslkahdkijwhlfkdiuhkjawdhsasdfghjk</p>
+		</div>
+	</section>
+	<section style="background-color: pink;">
+        <div class="container text">
+			<p>dkslhdjsahdalskdaskjdskaldhnjslkahdkijwhlfkdiuhkjawdhsasdfghjk</p>
+			<p>dkslhdjsahdalskdaskjdskaldhnjslkahdkijwhlfkdiuhkjawdhsasdfghjk</p>
+		</div>
+	</section>
+	
+<?php include "footer.php";?>
+</body>
+</html>                                		
