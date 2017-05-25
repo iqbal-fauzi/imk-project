849b32a6992e66a1e057de0d9b5be87107f52230
ASUS <ASUS@ASUS-PC>
2017-05-25 22:55:48 +0700

    # setting backed admin

---
+++ adminlte/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'header.php'; ?>
+  <!-- Left side column. contains the logo and sidebar -->
+  <?php include 'left.php'; ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+      dkjlsakj
+      <?php echo 'hehe'; ?>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
---
+++ adminlte/left.php
@@ -0,0 +1,8 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      
+    </section>
+    <!-- /.sidebar -->
+  </aside>
